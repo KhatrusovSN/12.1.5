31b3415180329da3ddbf72a0560c1891e22fdfd7
KhatrusovSN <113129616+KhatrusovSN@users.noreply.github.com>
2022-11-25 20:59:59 +0300

    Добавьте файлы проекта.

---
+++ 12.1.5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _12._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание списка пользователей.
+            List<User> users = new List<User>()
+            {
+                new User{ Login = "qwerty1", Name = "Петр Петров", IsPremium = true },
+                new User{ Login = "qwerty2", Name = "Александр Александров", IsPremium = false },
+                new User{ Login = "qwerty3", Name = "Сергей Николаевич", IsPremium = false }
+            };
+
+            Console.Write("Enter login: ");
+            string login = Console.ReadLine();
+
+            foreach (User user in users)
+            {
+                if (user.Login == login)
+                {
+                    Console.WriteLine($"Hello, {user.Name}");
+
+                    if (!user.IsPremium)
+                    {
+                        User.ShowAds();
+                    }
+                }
+            }
+        }
+    }
+}
